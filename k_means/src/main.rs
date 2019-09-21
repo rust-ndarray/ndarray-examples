f4e0bd32d195617c93f80f3510472ff37593e7a1
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-09-21 17:56:27 +0100

    Add comments

--- k_means/src/main.rs
+++ k_means/src/main.rs
@@ -1,24 +1,27 @@
 #![allow(non_snake_case)]
-use ndarray::{Array, Array1, Array2, Axis};
+use ndarray::{Array, Array2, Axis, stack};
 use ndarray_rand::RandomExt;
-use ndarray_stats::DeviationExt;
-use rand::distributions::{Normal, StandardNormal};
+use rand::distributions::Normal;
 
 // Import KMeans from other file ("lib.rs") in this example
 use k_means::KMeans;
 
 /// It returns a data distribution.
 ///
+/// The data is clearly centered around two distinct points,
+/// to quickly spot if something is wrong with the KMeans algorithm
+/// looking at the output.
 fn get_data(n_samples: usize, n_features: usize) -> Array2<f64> {
-    let shape = (n_samples, n_features);
-    let X: Array2<f64> = Array::random(shape, StandardNormal);
-    X
+    let shape = (n_samples / 2, n_features);
+    let X1: Array2<f64> = Array::random(shape, Normal::new(1000., 0.1));
+    let X2: Array2<f64> = Array::random(shape, Normal::new(-1000., 0.1));
+    stack(Axis(0), &[X1.view(), X2.view()]).unwrap().to_owned()
 }
 
 pub fn main() {
-    let n_samples = 5000;
+    let n_samples = 5000000;
     let n_features = 3;
-    let n_clusters = 2;
+    let n_clusters = 8;
 
     let X = get_data(n_samples, n_features);
 
